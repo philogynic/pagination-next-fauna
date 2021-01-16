6ba81aadbd887af502e21308154b7c180e1910d1
philogynic <bimogame161@gmail.com>
2021-01-17 00:09:03 +0700

    getting the after

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -2,19 +2,24 @@ import {query as q} from 'faunadb'
 import {serverClient} from '../../../utils/fauna-auth'
 
 export default async (req, res) => {
+
+    const {limit, cursor} = req.query
+    console.log(limit, cursor)
     try {
         const users = await serverClient.query(
             q.Map(
                 q.Paginate(
                     q.Match(
                         q.Index('all_users')
-                    )
+                    ), {size: parseInt(limit)}
                 ),
                 (ref) => q.Get(ref)
             )
         )
-        res.status(200).json(users.data)
+        
+        // res.status(200).send({data: users.data, after: users.after})
+        res.status(200).send(users)
     } catch (e) {
-        res.status(500).json({error: e.message})
+        res.status(500).send({error: e.message})
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import {useForm} from 'react-hook-form'
 import {useState} from 'react'
-import useSWR from 'swr'
+import useSWR, { useSWRInfinite } from 'swr'
 import Router from 'next/router'
 
 const faker = require('faker')
+
 const fetcher = (url) => fetch(url).then((r) => r.json())
+
+async function swrFetcher(path) {
+  const res = await fetch(path)
+  return res.json()
+}
+
 
 export default function Home() {
 
@@ -42,9 +49,23 @@ export default function Home() {
     }
   })
 
-  const {data, error} = useSWR('/api/users', fetcher)
+  const limit = 6
+  // const cursor = 'abc'
 
-  if (error) return <div>failed to load</div>
+  const getKey = (pageIndex, previousPageData) => {
+
+    if (previousPageData && !previousPageData.data) return null
+
+    if (pageIndex === 0) return `/api/users?limit=10`
+
+    return `/api/users?limit=5&cursor=${previousPageData.after}`
+  }
+
+  // const {data, mutate, size, setSize} = useSWRInfinite(getKey, swrFetcher)
+
+
+
+  const {data: users, error} = useSWR(`/api/users?limit=5&cursor=abc`, fetcher)
 
   return (
     <div className='font-mono'>
@@ -64,19 +85,48 @@ export default function Home() {
         </form>
 
         <div className='flex-1 flex flex-col bg-gray-100 overflow-y-auto'>
-          {data ? (
-            data.map((d) => {
+          {/* {data ? (
+            data.data.map((d, index) => {
               return (
                 <div 
-                  className='flex p-2 m-1 bg-white rounded-lg'
+                  className='flex p-6 m-1 bg-white rounded-lg'
                   key={d.ref['@ref'].id}
-                >{d.data.name}</div>
+                >{index} {d.data.name}</div>
+                
               )
-            }) 
+            })
+            (<div>{data.after}</div>)
+          ) : (
+            <div>loading...</div>
+          )} */}
+
+        {users ? (
+            users.data.map((d, index) => {
+              return (
+                <div 
+                  className='flex p-6 m-1 bg-white rounded-lg'
+                  key={d.ref['@ref'].id}
+                >{index} {d.data.name}</div>
+                
+              )
+            })
           ) : (
             <div>loading...</div>
           )}
+
+          {users ? (
+            users.after.map((d) => {
+              return (
+                <div>{d['@ref'].id}</div>
+              )
+            })
+          ) : (
+            <div>loading the after...</div>
+          )}
+
+          <button className='bg-blue-200'>Load more...</button>
         </div>
+
       </main>
     </div>
   )
